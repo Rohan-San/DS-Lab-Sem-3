427555a93e72bd7e87a0412594462d44c9329fa5
Rohan Santhosh <107957865+Rohan-San@users.noreply.github.com>
2022-12-07 12:44:10 +0530

    Update queue.c

--- queue.c
+++ queue.c
@@ -6,9 +6,7 @@
 void insert();
 void delete();
 void display();
-int queue_array[MAX];
-int rear = - 1;
-int front = - 1;
+int queue_array[MAX],rear=-1,front=-1;
 
 void main()
 {
@@ -42,44 +40,44 @@ void main()
 
 void insert()
 {
-int add_item;
-if (rear == MAX - 1)
-printf("Queue Overflow \n");
-else
-{
-    if (front == - 1)
-    front = 0;
-    printf("Inset the element in queue : ");
-    scanf("%d", &add_item);
-    rear = rear + 1;
-    queue_array[rear] = add_item;
+	int add_item;
+	if (rear == MAX - 1)
+	printf("Queue Overflow \n");
+	else
+	{
+		if (front == - 1)
+		front = 0;
+		printf("Inset the element in queue : ");
+		scanf("%d", &add_item);
+		rear = rear + 1;
+		queue_array[rear] = add_item;
+	}
 }
-} /* End of insert() */
 
 void delete()
 {
-if (front == - 1 || front > rear)
-{
-    printf("Queue Underflow \n");
-    return ;
+	if (front == - 1 || front > rear)
+	{
+		printf("Queue Underflow \n");
+		return ;
+	}
+	else
+	{
+		printf("Element deleted from queue is : %d\n", queue_array[front]);
+		front = front + 1;
+	}
 }
-else
-{
-    printf("Element deleted from queue is : %d\n", queue_array[front]);
-    front = front + 1;
-}
-} /* End of delete() */
 
 void display()
 {
-int i;
-if (front == - 1)
-    printf("Queue is empty \n");
-else
-{
-    printf("Queue is : \n");
-    for (i = front; i <= rear; i++)
-        printf("%d ", queue_array[i]);
-    printf("\n");
-}
+	int i;
+	if (front == - 1)
+		printf("Queue is empty \n");
+	else
+	{
+		printf("Queue is : \n");
+		for (i = front; i <= rear; i++)
+		    printf("%d ", queue_array[i]);
+		printf("\n");
+	}
 }
